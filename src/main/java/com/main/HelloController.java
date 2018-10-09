e488ca76ad1e963bb693a8b8e204e75a189adcc2
tom <tianyacsdn@126.com>
2018-10-09 17:48:15 +0800

    HelloControllerTest

--- src/main/java/com/main/HelloController.java
+++ src/main/java/com/main/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @RequestMapping("/hello")
     public String index(){
         return "hello world ! Spring Boot 2.0.5";
--- src/test/java/com/mian/HelloControllerTest.java
+++ src/test/java/com/mian/HelloControllerTest.java
@@ -1,4 +1,37 @@
 package com.mian;
 
+import com.main.ApplicationMain;
+import com.main.HelloController;
+import javafx.application.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ApplicationMain.class)
 public class HelloControllerTest {
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        System.out.println("-----------------------");
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("hello world ! Spring Boot 2.0.5")));
+    }
 }
